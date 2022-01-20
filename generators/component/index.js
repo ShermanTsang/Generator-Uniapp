8bbd03ef412e0604ed9f1df94b3326915ca5c0a5
ShareManT <sharemant@163.com>
2022-01-20 19:38:58 +0800

    feat(template): add component generator template

---
+++ generators/component/index.js
@@ -0,0 +1,21 @@
+const Generator = require("yeoman-generator");
+const _ = require("lodash");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("componentName", { type: String, required: true });
+  }
+
+  writing() {
+    let { componentName } = this.options;
+    componentName = _.upperFirst(_.camelCase(componentName));
+    const className = _.kebabCase(componentName);
+    this.options.className = className;
+    this.fs.copyTpl(
+      this.templatePath("component.vue"),
+      this.destinationPath(`src/components/${className}/${className}.vue`),
+      this.options
+    );
+  }
+};
---
+++ generators/component/templates/page.vue
@@ -0,0 +1,32 @@
+<template>
+  <view class="<%= className %>"></view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+interface DataType extends Partial<Code.BaseDataType> {
+  config: {};
+  data: {};
+  state: {};
+}
+
+export default Vue.extend({
+  name: '<%= componentName %>',
+  data(): DataType {
+    return {
+      config: {},
+      data: {},
+      state: {}
+    };
+  },
+  props: {},
+  methods: {},
+  computed: {},
+});
+</script>
+
+<style lang="scss" scoped>
+.<%= className %> {
+}
+</style>
