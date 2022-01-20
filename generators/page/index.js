cf440f33d0c0a2be09961ab3f31a87bd1c0027c9
ShareManT <sharemant@163.com>
2022-01-20 19:36:45 +0800

    feat(template): add page generator template

---
+++ generators/page/index.js
@@ -0,0 +1,66 @@
+const _ = require("lodash");
+const fs = require("fs");
+const Generator = require("yeoman-generator");
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("pageName", { type: String, required: true });
+  }
+
+  writing() {
+    let { pageName } = this.options;
+    let pathStr = "";
+    if (pageName.includes("/")) {
+      const arr = pageName.split("/");
+      pageName = _.last(arr);
+      pathStr = arr.filter((item, key) => key < arr.length - 1);
+    }
+
+    pageName = _.kebabCase(pageName);
+    this.options.className = _.kebabCase(pageName);
+    this.options.componentName = _.upperFirst(_.camelCase(pageName));
+    this.fs.copyTpl(
+      this.templatePath("page.vue"),
+      this.destinationPath(`src/pages/${pathStr}/${pageName}/${pageName}.vue`),
+      this.options
+    );
+
+    // Register router to pages.json
+    if (this.options.route) {
+      const file = this.destinationPath("src/pages.json");
+      let page = fs.readFileSync(file, "utf-8");
+      if (!page) {
+        page = JSON.stringify({
+          pages: []
+        });
+      }
+
+      try {
+        let path = `pages/${pageName}/${pageName}`;
+        if (pathStr) {
+          path = `pages/${pathStr}/${pageName}/${pageName}`;
+        }
+
+        page = JSON.parse(page);
+        const paths = [];
+        page.pages.forEach(item => {
+          paths.push(item.path);
+        });
+        if (paths.includes(path)) {
+          return this.log.error(`The Path ${path} is already exists`);
+        }
+
+        page.pages.push({
+          path,
+          style: {
+            navigationBarTitleText: this.options.title ? this.options.title : ""
+          }
+        });
+        fs.writeFileSync(file, JSON.stringify(page, null, 2));
+      } catch (e) {
+        throw e;
+      }
+    }
+  }
+};
---
+++ generators/page/templates/page.vue
@@ -0,0 +1,38 @@
+<template>
+  <view class="<%= className %>"></view>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+interface DataType extends Partial<Code.BaseDataType> {
+  config: {};
+  data: {};
+  state: {};
+}
+
+export default Vue.extend({
+  name: '<%= componentName %>',
+  data(): DataType {
+    return {
+      config: {},
+      data: {},
+      state: {}
+    };
+  },
+  methods: {},
+  computed: {},
+  onLoad() {
+  },
+  onPullDownRefresh() {
+    Promise.all([]).then(() => uni.stopPullDownRefresh());
+  },
+  onReachBottom() {
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.<%= className %> {
+}
+</style>
