5a8fb10939fb76fcfa32e13225e372cad0b64e07
ShareManT <sharemant@163.com>
2022-01-20 19:20:52 +0800

    feat(template): add app template index

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,53 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+const yosay = require("yosay");
+const path = require("path");
+const fs = require("fs");
+
+const Generator = require("yeoman-generator");
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the ${chalk.red('uni-app')} generator!`
-      )
-    );
-
-    const prompts = [
+    this.log(yosay("Welcome to the uniapp generator!"));
+    return this.prompt([
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: "input",
+        name: "name",
+        message: "The name of your project?",
+        default: this.appname
       }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    ]).then(answers => {
+      this.answers = answers;
     });
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+  wirting() {
+    const tmpDir = this.templatePath();
+    const context = this.answers;
+    console.log(tmpDir);
+    const tempList = [];
 
-  install() {
-    this.installDependencies();
+    function fileDisplay(filePath) {
+      const files = fs.readdirSync(filePath);
+      files.forEach(function(filename) {
+        const fileDir = path.join(filePath, filename);
+        const stats = fs.statSync(fileDir);
+        if (stats.isFile()) {
+          tempList.push(fileDir);
+        }
+
+        if (stats.isDirectory()) {
+          fileDisplay(fileDir);
+        }
+      });
+    }
+
+    fileDisplay(tmpDir);
+    tempList.forEach(file => {
+      const source = file.replace(this.templatePath() + "/", "");
+      this.fs.copyTpl(
+        this.templatePath(source),
+        this.destinationPath(source),
+        context
+      );
+    });
   }
 };
