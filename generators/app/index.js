5b1d6beb2da4b2b0349418a78401e1be7c548583
ShareManT <sharemant@163.com>
2022-01-21 21:25:18 +0800

    feat: update app template configuration

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,10 +2,14 @@ const yosay = require("yosay");
 const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
-
 const Generator = require("yeoman-generator");
+const mkdirp = require("mkdirp");
 
 module.exports = class extends Generator {
+  initializing() {
+    this.props = {};
+  }
+
   prompting() {
     this.log(yosay(`Welcome to the ${chalk.red("sherman-uniapp")} generator!`));
     const prompts = [
@@ -18,39 +22,66 @@ module.exports = class extends Generator {
     ];
 
     return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
       this.props = props;
     });
   }
 
-  writing() {
-    const tmpDir = this.templatePath();
-    const context = this.props;
-    console.log(tmpDir);
-    const tempList = [];
+  default() {
+    this.log(
+      `${chalk.bgGreen(
+        "[ Info ]"
+      )} find default destination path is ${chalk.blue(this.destinationPath())}`
+    );
+    this.log(
+      `${chalk.bgGreen(
+        "[ Info ]"
+      )} find generator templates path is ${chalk.blue(this.templatePath())}`
+    );
+    if (path.basename(this.destinationPath()) !== this.props.name) {
+      mkdirp(this.props.name);
+      this.log(
+        `${chalk.bgGreen("[ Info ]")} create ${chalk.blue(
+          this.destinationPath()
+        )} director`
+      );
+      this.destinationRoot(this.destinationPath(this.props.name));
+      this.log(
+        `${chalk.bgYellow("[ Notice ]")} set destination path to ${chalk.blue(
+          this.destinationPath()
+        )}`
+      );
+    }
+  }
 
-    function fileDisplay(filePath) {
+  writing() {
+    const templateFiles = [];
+
+    function fileMapper(filePath) {
       const files = fs.readdirSync(filePath);
       files.forEach(function(filename) {
         const fileDir = path.join(filePath, filename);
         const stats = fs.statSync(fileDir);
         if (stats.isFile()) {
-          tempList.push(fileDir);
+          templateFiles.push(fileDir);
         }
 
         if (stats.isDirectory()) {
-          fileDisplay(fileDir);
+          fileMapper(fileDir);
         }
       });
     }
 
-    fileDisplay(tmpDir);
-    tempList.forEach(file => {
-      const source = file.replace(this.templatePath() + "/", "");
+    fileMapper(this.templatePath());
+
+    templateFiles.forEach(filePath => {
+      const fileBarePath = filePath.replace(
+        path.normalize(this.templatePath() + "/"),
+        ""
+      );
       this.fs.copyTpl(
-        this.templatePath(source),
-        this.destinationPath(source),
-        context
+        this.templatePath(fileBarePath),
+        this.destinationPath(fileBarePath),
+        this.props
       );
     });
   }
