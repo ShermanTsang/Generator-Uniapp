d30805c516fcf8f411a06069d7c9b51db72d398a
ShareManT <sharemant@163.com>
2022-01-28 17:27:34 +0800

    chore: separate two different version

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,92 +1,108 @@
-const yosay = require("yosay");
-const path = require("path");
-const fs = require("fs");
-const chalk = require("chalk");
-const Generator = require("yeoman-generator");
-const mkdirp = require("mkdirp");
+const yosay = require('yosay');
+const path = require('path');
+const fs = require('fs');
+const chalk = require('chalk');
+const Generator = require('yeoman-generator');
+const mkdirp = require('mkdirp');
 
 module.exports = class extends Generator {
-  initializing() {
-    this.props = {};
-  }
-
-  prompting() {
-    this.log(yosay(`Welcome to the ${chalk.red("sherman-uniapp")} generator!`));
-    const prompts = [
-      {
-        type: "input",
-        name: "name",
-        message: "The name of your project?",
-        default: this.appname
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      this.props = props;
-    });
-  }
-
-  default() {
-    this.log(
-      `${chalk.bgGreen(
-        "[ Info ]"
-      )} find default destination path is ${chalk.blue(this.destinationPath())}`
-    );
-    this.log(
-      `${chalk.bgGreen(
-        "[ Info ]"
-      )} find generator templates path is ${chalk.blue(this.templatePath())}`
-    );
-    if (path.basename(this.destinationPath()) !== this.props.name) {
-      mkdirp(this.props.name);
-      this.log(
-        `${chalk.bgGreen("[ Info ]")} create ${chalk.blue(
-          this.destinationPath()
-        )} director`
-      );
-      this.destinationRoot(this.destinationPath(this.props.name));
-      this.log(
-        `${chalk.bgYellow("[ Notice ]")} set destination path to ${chalk.blue(
-          this.destinationPath()
-        )}`
-      );
-    }
-  }
-
-  writing() {
-    const templateFiles = [];
-
-    function fileMapper(filePath) {
-      const files = fs.readdirSync(filePath);
-      files.forEach(function(filename) {
-        const fileDir = path.join(filePath, filename);
-        const stats = fs.statSync(fileDir);
-        if (stats.isFile()) {
-          templateFiles.push(fileDir);
-        }
-
-        if (stats.isDirectory()) {
-          fileMapper(fileDir);
-        }
-      });
+    initializing() {
+        this.props = {};
+        this.status = {
+            templatePathExisted: true
+        };
     }
 
-    fileMapper(this.templatePath());
+    prompting() {
+        this.log(yosay(`Welcome to the ${chalk.red('sherman-uniapp')} generator!\n`));
+        const prompts = [
+            {
+                type: 'input',
+                name: 'name',
+                message: 'The name of your project?',
+                default: this.appname
+            },
+            {
+                type: 'list',
+                name: 'version',
+                message: 'Which version and boilerplate of uni-app you wanna use?',
+                default: 'v2',
+                choices: [
+                    {
+                        key: 'v2',
+                        name: 'Vue2.6 + TypeScript + Webpack',
+                        value: 'v2'
+                    },
+                    {
+                        key: 'v3',
+                        name: 'Vue3.x + TypeScript + Vite',
+                        value: 'v3'
+                    }
+                ]
+            }
+        ];
 
-    templateFiles.forEach(filePath => {
-      const fileBarePath = filePath.replace(
-        path.normalize(this.templatePath() + "/"),
-        ""
-      );
-      this.fs.copyTpl(
-        this.templatePath(fileBarePath),
-        this.destinationPath(fileBarePath),
-        this.props
-      );
-    });
-  }
+        return this.prompt(prompts).then((props) => {
+            this.props = props;
+        });
+    }
 
-  install() {
-    this.installDependencies();
-  }
+    default() {
+        this.log(`${chalk.bgGreen('[ Info ]')} default destination path is ${chalk.blue(this.destinationPath())}\n`);
+        this.log(`${chalk.bgGreen('[ Info ]')} generator templates path is ${chalk.blue(this.templatePath())}\n`);
+
+        if (this.props.version === 'v2') {
+            if (path.basename(this.destinationPath()) !== this.props.name) {
+                mkdirp(this.props.name);
+                this.log(`${chalk.bgGreen('[ Info ]')} create ${chalk.blue(this.destinationPath())} director`);
+                this.destinationRoot(this.destinationPath(this.props.name));
+                this.log(`${chalk.bgYellow('[ Notice ]')} set destination path to ${chalk.blue(this.destinationPath())}`);
+            }
+        }
+
+        if (this.props.version === 'v3') {
+            this.log(`${chalk.bgGreen('[ Warning ]')} version3 is under construction\n`);
+        }
+    }
+
+    writing() {
+        const templatePath = path.normalize(this.templatePath() + '/' + this.props.version);
+        this.log(`${chalk.bgGreen('[ Info ]')} target template director is ${chalk.blue(templatePath)}\n`);
+
+        try {
+            fs.accessSync(templatePath);
+            const templateFiles = [];
+
+            function fileMapper(filePath) {
+                const files = fs.readdirSync(filePath);
+                files.forEach(function(filename) {
+                    const fileDir = path.join(filePath, filename);
+                    const stats = fs.statSync(fileDir);
+                    if (stats.isFile()) {
+                        templateFiles.push(fileDir);
+                    }
+
+                    if (stats.isDirectory()) {
+                        fileMapper(fileDir);
+                    }
+                });
+            }
+
+            fileMapper(templatePath);
+
+            templateFiles.forEach((filePath) => {
+                const fileBarePath = filePath.replace(path.normalize(this.templatePath() + '/'), '');
+                this.fs.copyTpl(this.templatePath(fileBarePath), this.destinationPath(path.normalize(fileBarePath.replace(this.props.version, './'))), this.props);
+            });
+        } catch {
+            this.log(`${chalk.bgRed('[ Error ]')} target template director is ${chalk.blue(templatePath)} not existed\n`);
+            this.status.templatePathExisted = false;
+        }
+    }
+
+    install() {
+        if (this.status.templatePathExisted) {
+            this.installDependencies();
+        }
+    }
 };
